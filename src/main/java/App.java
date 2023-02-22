35d17a7d5db6d014d83f120e802e179fea30b4d0
Abhishek-Pandey9083 <72137693+Abhishek-Pandey9083@users.noreply.github.com>
2023-02-22 16:51:43 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("I am your awesome Java  and it is good!");
+         System.out.println("it is working !");
 
         DeepThought ourSuperComputer = new DeepThought();
         int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
